c6ff900b0c81c982f9105ec9125734bab368368f
Leonard Akinmolayan <lakinmolayan@gmail.com>
2026-03-30 22:13:19 +0200

    chore: - fix for conflicts and reintroduced lsp config

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,6 @@
 local setup = function()
  require("nvim-treesitter.configs").setup{
-<<<<<<< HEAD
-   ensure_installed = {"markdown", "vimdoc", "lua", "rust", "toml", "scala" },
-=======
    ensure_installed = {"markdown", "vimdoc", "lua", "rust", "toml", "scala", "cmake", "c", "kotlin", "go"},
->>>>>>> 1d768fc (Clean up configs)
    highlight = {
     enable = true
   }
